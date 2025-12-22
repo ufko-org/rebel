8d75047e0d7f5ec1bb0c326cf2ec2def90b02ba0
ufko <ufko@gringo.home>
2025-12-22 16:41:00 +0100

    Improve comment for new/retired primes

--- src/primes.h
+++ src/primes.h
@@ -89,11 +89,12 @@ PRIMITIVE primitive[] =
     {"expand",      p_expand,   0},
     {"constant",    p_constant, 0x400},
     {"global",      p_global,   0},
-    /* set retires set '/setf/setq */
-    {"set",         p_setdef,      0x400}, /* rebel origin */ 
-    /* {"set",         p_set,      0x400}, */
-    /* {"setf",        p_setf,     0x400}, */
-    /* {"setq",        p_setf,     0x400}, */
+    {"set",         p_setdef,      0x400}, /* rebel origin: retires set '/setq/setf */ 
+    /* retired from language:
+    {"set",         p_set,      0x400},
+    {"setf",        p_setf,     0x400},
+    {"setq",        p_setf,     0x400},
+    */
     {"let",         p_let,      0x402},
     {"local",       p_local,    2},
     {"letn",        p_letn,     0x002},
